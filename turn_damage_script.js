2e0b2fff080d9865b929d528ce7a8fe2ed5ec4d8
KOHTPOJIEP <death.for.enemy@gmail.com>
2022-01-09 01:21:07 +0300

    Added chat reading to grab LMRTFY result

--- turn_damage_script.js
+++ turn_damage_script.js
@@ -1,3 +1,54 @@
+function get_roll_total_from_msg()
+{
+	let messages = game.messages._source
+	let content = messages[messages.length - 1].content
+	let el = document.createElement('html');
+	el.innerHTML = content
+	let tag_inner = el.getElementsByTagName('h4')[0].innerHTML
+	let value = parseFloat(tag_inner.substring(0, tag_inner.indexOf('<span')))
+	return value
+}
+
+function are_more_msgs(len_msgs_before)
+{
+    if (game.messages._source.length > len_msgs_before) {return 1}
+    else {return 0}
+}
+
+const asyncInterval = async (msgs_before, ms, triesLeft = 5) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      if (are_more_msgs(msgs_before) > 0) {
+        resolve(get_roll_total_from_msg());
+        clearInterval(interval);
+      } else if (triesLeft <= 1) {
+        reject(0);
+        clearInterval(interval);
+      }
+      triesLeft--;
+      console.log(`Tries: ${triesLeft}, length: ${game.messages._source.length}`)
+    }, ms);
+  });
+}
+
+function deal_with_condition(save_roll_total, dc, damage_type, attacking_actor, my_item, target_token, alert_name, damage_roll)
+{
+console.log(`Roll: ${save_roll_total}, DC: ${dc}, damage type: ${damage_type}`)
+let flav = `${attacking_actor.name} наносит ${damage_type} урон при ударе ${my_item.name} по ${target_token.actor.name} with \n`
+let saved_msg = `${target_token.actor.name} избавляется от <b>${alert_name}</b>!`
+if (save_roll_total < dc) {
+new MidiQOL.DamageOnlyWorkflow(attacking_actor._id, attacking_actor.token._id, damage_roll.total, damage_type, [target_token], damage_roll, {flavor: flav, itemCardId: my_item.itemCardId});
+}
+else {
+   if (game.cub.hasCondition(alert_name, target_token)) {game.cub.removeCondition(alert_name, target_token)}
+   let alert = TurnAlert.getAlertByName(alert_name, combat_id)
+   console.log(`Alert id: ${alert.id}, combat: ${alert.combatId}, name: ${alert.name}`)
+   TurnAlert.delete(alert.combatId, alert.id)
+   ChatMessage.create({user: null, speaker: ChatMessage.getSpeaker({token: target_token}), 
+                       content:saved_msg});
+}
+}
+
 let attacking_actor = args[0]
 let my_item = args[1]
 let target_token = Array.from(game.user.targets)[0]
@@ -9,42 +60,7 @@ let save_ability = args[6]
 let damage_formula = args[7]
 let damage_roll = new Roll(damage_formula).roll()
 console.log(`Save ability: ${save_ability}`)
-//LMRTFYRoller.requestSavingThrows(target_token.actor, "dex");
-
-//messages = game.ChatLog.collection
-//data = game.data
-//messages = game.messages._source
-//last_message = messages[messages.length - 1]
-//save_value = last_message.roll.total
-//console.log(`inner_script ${Object.getOwnPropertyNames(game.messages._source)}, type ${game.messages._source}`)
-let save_modifier = 0
-
-if (save_ability == 'dex') 
-{
-    console.log(`Reached dex branch`)
-    save_modifier = target_token.actor.data.data.abilities.dex.save
-	console.log(`Modifier: ${target_token.actor.data.data.abilities.dex.save}, ${save_modifier}`)
-}
-
-if (save_ability == 'con') 
-{
-    console.log(`Reached CON branch`)
-    save_modifier = target_token.actor.data.data.abilities.con.save
-	console.log(`Modifier: ${target_token.actor.data.data.abilities.con.save}, ${save_modifier}`)
-}
-
-let save_roll = new Roll(`d20+${save_modifier}`).roll()
-console.log(`Roll: ${save_roll.total}, DC: ${dc}, damage type: ${damage_type}`)
-let flav = `${attacking_actor.name} наносит ${damage_type} урон при ударе ${my_item.name} по ${target_token.actor.name} with \n`
-let saved_msg = `${target_token.actor.name} избавляется от <b>${alert_name}</b>!`
-if (save_roll.total < dc) {
-new MidiQOL.DamageOnlyWorkflow(attacking_actor._id, attacking_actor.token._id, damage_roll.total, damage_type, [target_token], damage_roll, {flavor: flav, itemCardId: my_item.itemCardId});
-}
-else {
-   if (game.cub.hasCondition(alert_name, target_token)) {game.cub.removeCondition(alert_name, target_token)}
-   let alert = TurnAlert.getAlertByName(alert_name, combat_id)
-   console.log(`Alert id: ${alert.id}, combat: ${alert.combatId}, name: ${alert.name}`)
-   TurnAlert.delete(alert.combatId, alert.id)
-   ChatMessage.create({user: null, speaker: ChatMessage.getSpeaker({token: target_token}), 
-                       content:saved_msg});
-}
+LMRTFYRoller.requestSavingThrows(target_token.actor, save_ability);
+const len_msgs_before = game.messages._source.length
+console.log(`Msgs before ${len_msgs_before}`)
+asyncInterval(len_msgs_before, 1000).then(resolve => {deal_with_condition(resolve, dc, damage_type, attacking_actor, my_item, target_token, alert_name, damage_roll)})
